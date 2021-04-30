c35242ea401bc04e14b562a60efe1cc3dfb1785a
Gioza <karagiozov.a@mail.ru>
2021-05-01 00:10:54 +0400

    Added 3 task

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -40,40 +40,63 @@
 
 
 
-function getAverageScore(data) {
-    let obj = {};
-    let aveOfData = 0;
-    for(let subject in data) {
-        if(data[subject].length>0){
-        aveOfData = getAverageMark(data[subject]);
-        obj[subject] = aveOfData;
-        }
-    }
-    let aveOfObj = 0;
-    for(let subject in obj) {
-        aveOfObj += obj[subject];
-    }
-    if(Object.keys(obj).length>0){
-    aveOfObj /= Object.keys(obj).length;
-    }
-    obj["average"] = aveOfObj;
+// function getAverageScore(data) {
+//     let obj = {};
+//     let aveOfData = 0;
+//     for(let subject in data) {
+//         if(data[subject].length>0){
+//         aveOfData = getAverageMark(data[subject]);
+//         obj[subject] = aveOfData;
+//         }
+//     }
+//     let aveOfObj = 0;
+//     for(let subject in obj) {
+//         aveOfObj += obj[subject];
+//     }
+//     if(Object.keys(obj).length>0){
+//     aveOfObj /= Object.keys(obj).length;
+//     }
+//     obj["average"] = aveOfObj;
+//     return obj;
+// }
+
+
+// let grades = {  algebra: [2, 5, 3, 4, 4, 3],
+//                 geometry: [5, 4, 3, 5, 4,],
+//                 history:[4, 5, 5, 5, 4,]};
+
+
+
+// getAverageScore(grades);
+
+// function getAverageMark(marks) {
+//     let aveOfMarks = 0;
+//     for(let i = 0; i < marks.length; i++) {
+//         aveOfMarks += marks[i];
+//     }
+//     aveOfMarks = aveOfMarks / marks.length;
+//     return aveOfMarks;
+// }
+
+
+let data = {aaa: 0,
+            bbb: 1}
+
+
+function getPersonData(secretData) {
+    let obj = {}
+    obj['firstName'] = getDecodedValue(secretData['aaa']);
+    obj['lastName'] = getDecodedValue(secretData['bbb']);
     return obj;
 }
 
-
-let grades = {  algebra: [2, 5, 3, 4, 4, 3],
-                geometry: [5, 4, 3, 5, 4,],
-                history:[4, 5, 5, 5, 4,]};
+getPersonData(data);
 
 
-
-getAverageScore(grades);
-
-function getAverageMark(marks) {
-    let aveOfMarks = 0;
-    for(let i = 0; i < marks.length; i++) {
-        aveOfMarks += marks[i];
+function getDecodedValue(secret) {
+    if(secret == 0) {
+        return 'Родриго';
+    } else {
+        return 'Эмильо';
     }
-    aveOfMarks = aveOfMarks / marks.length;
-    return aveOfMarks;
 }
